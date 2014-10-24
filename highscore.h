7934f8a2218fc28d588757dd2725fbb420669782
Matt <msw102394@gmail.com>
2014-10-24 14:18:32 -0400

    I have no idea what I'm doing

--- highscore.h
+++ highscore.h
@@ -1,10 +1,42 @@
 #ifndef HIGHSCORE_H
 #define HIGHSCORE_H
 
-class HighScore
-{
+class Ship {
+
+private:
+    int x, y;  // position
+    int fuelLevel;  // amount of fuel
+    int shields; // shield level
+
+    Ship(int initShields) {
+            // instantiate ship
+    }
+
 public:
-    HighScore();
+    void fly(int direction, int distance) {
+        // moves ship
+    }
+
+    void refuel(int amount) {
+        // refuels ship
+    }
+
+/***** singleton implementation ****/
+private:
+    static Ship *instance;
+
+public:
+    static Ship* getInstance() {
+        if (instance == nullptr) {
+            instance = new Ship(10);
+        }
+        return instance;
+    }
+
 };
 
-#endif // HIGHSCORE_H
+Ship Ship::instance = nullptr;
+
+int main() {
+    Ship::getInstance()->fly(3, 5);
+}
