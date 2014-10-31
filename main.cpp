3ee92a08244b85a4bf1ff7dda9b7f42a2549fd65
Joshua Donahue <jtdonahue360@gmail.com>
2014-10-31 11:54:11 -0400

    added function to GameModel that returns string state.  Also removed old comments

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
 #include <QApplication>
+#include <QDebug>
+#include <QString>
 
 #include "mainwindow.h"
 #include "gamemodel.h"
@@ -7,12 +9,12 @@
 
 
 bool unitTest(){
-    //GameModel* model = new GameModel(500,500);
     GameModel::getInstance().initializeGame();
     GameModel::getInstance().create("enemy",1,2);
     GameModel::getInstance().create("enemy",2,3);
     GameModel::getInstance().create("enemy",34,5);
     GameModel::getInstance().saveGame("savefilename");
+    qDebug(GameModel::getInstance().state().c_str());
 
     GameModel::getInstance().reset();
 
